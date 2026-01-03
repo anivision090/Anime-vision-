c422cdb4d1954e043b903af9dd361a79134fbaad
anivision090 <seemahaider1430909@gmail.com>
2026-01-03 11:03:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
-  <React.StrictMode>
-    <h1 style={{ color: "white", textAlign: "center" }}>
-      Delwa React App Started ✅
-    </h1>
-  </React.StrictMode>
-);
+root.render(<App />);

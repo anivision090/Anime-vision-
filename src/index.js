a69e7931dbf9a105cae2e38549b87adad23f2ad5
anivision090 <seemahaider1430909@gmail.com>
2026-01-03 09:34:41 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <h1 style={{ color: "white", textAlign: "center" }}>
+      Delwa React App Started ✅
+    </h1>
+  </React.StrictMode>
+);
